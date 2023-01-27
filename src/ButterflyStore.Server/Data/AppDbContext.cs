c63c4b8cd163db833bf8af6181c7efa98094c2c4
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-01-27 14:55:19 +0300

    Created the application db context class

---
+++ src/ButterflyStore.Server/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using ButterflyStore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ButterflyStore.Server.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<Cloth> CLothes { get; set; } = null!;
+}
