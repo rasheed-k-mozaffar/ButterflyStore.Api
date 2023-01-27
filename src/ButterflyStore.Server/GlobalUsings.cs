bc5f5fd0e98ccb6cfd7aac069d9ec3a2d8c30c53
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-01-27 15:27:37 +0300

    Marked important namespaces as global for refactoring

---
+++ src/ButterflyStore.Server/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using ButterflyStore.Data.Dtos;
+global using ButterflyStore.Server.Data;
+global using ButterflyStore.Server.Services.Interfaces;
+global using ButterflyStore.Data.Models;
+global using Microsoft.EntityFrameworkCore;
