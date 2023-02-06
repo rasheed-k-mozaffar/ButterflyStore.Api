fd9fac7b6ee2a39e4581d046841b750814c7a595
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-02-06 20:49:25 +0300

    Second Files Submit

---
+++ src/Client/Pages/Auth/Register.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+
+namespace ButterflyStore.Client.Pages.Auth
+{
+	public partial class Register : ComponentBase
+	{
+		[Inject]
+		private HttpClient HttpClient { get; set; }
+
+
+
+
+		public Register()
+		{
+		}
+	}
+}
+
---
+++ src/Server/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using ButterflyStore.Shared;
+global using ButterflyStore.Shared.Enums; 
+global using ButterflyStore.Server.Data;
+global using ButterflyStore.Server.Services.Interfaces;
+global using ButterflyStore.Server.Services.Implementations;
+global using ButterflyStore.Data.Models;
+global using Microsoft.EntityFrameworkCore;
+global using ButterflyStore.Server.Exceptions;
+global using ButterflyStore.Shared.ApiResponses;
