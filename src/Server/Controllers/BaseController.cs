7a603910457613bab5bfc4e90f78692059f894b3
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-02-10 23:14:25 +0300

    Removed treblle to enhance the request processing speed

--- src/Server/Controllers/BaseController.cs
+++ src/Server/Controllers/BaseController.cs
@@ -6,7 +6,7 @@ namespace ButterflyStore.Server.Controllers;
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiController]
 [ApiVersion("1.0")]
-[Treblle]
+//[Treblle]
 public class BaseController : ControllerBase
 {
 
