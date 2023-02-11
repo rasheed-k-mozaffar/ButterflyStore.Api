e5a2104752af468035d4cde201757d6e1318fb91
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-02-11 15:27:05 +0300

    Implemented all USER and ROLE related endpoints

--- src/Server/Controllers/UserManagementController.cs
+++ src/Server/Controllers/UserManagementController.cs
@@ -27,7 +27,7 @@ namespace ButterflyStore.Server.Controllers
 		/// This endpoint returns all the roles.
 		/// </summary>
 		/// <returns></returns>
-		[HttpGet]
+		[HttpGet("GetRoles")]
 		public IActionResult GetAllRoles()
 		{
 			var roles = _roleManager.Roles.ToList();
@@ -36,11 +36,11 @@ namespace ButterflyStore.Server.Controllers
 		}
 
 		/// <summary>
-		/// This method takes a string in the query , and uses it to create a new role
+		/// This endpoint takes a string in the query , and uses it to create a new role
 		/// </summary>
 		/// <param name="roleName"></param>
 		/// <returns></returns>
-		[HttpPost]
+		[HttpPost("CreateRole")]
 		public async Task<IActionResult> CreateRole(string roleName)
 		{
 			//Check if the received role name is null or empty
@@ -74,7 +74,13 @@ namespace ButterflyStore.Server.Controllers
             }
 		}
 
-		[HttpDelete("{id}")]
+
+		/// <summary>
+		/// This endpoint deletes a role based on its ID
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpDelete("DeleteRole/{id}")]
 		public async Task<IActionResult> DeleteRole(string id)
 		{
 			//Check if the ID is null or empty
@@ -109,7 +115,7 @@ namespace ButterflyStore.Server.Controllers
 
 
 		/// <summary>
-		/// This method returns all the registered users inside our database.
+		/// This endpoint returns all the registered users inside our database.
 		/// </summary>
 		/// <returns></returns>
 		[HttpGet("GetUsers")]
@@ -121,6 +127,45 @@ namespace ButterflyStore.Server.Controllers
 		}
 
 
+		/// <summary>
+		/// This endpoint deletes a user based on its ID
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpDelete("DeleteUser/{id}")]
+		public async Task<IActionResult> DeleteUser(string id)
+		{
+            //Check if the ID is null or empty
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest("The ID is invalid"); //Return 400 BAD REQUEST STATUS CODE.
+            }
+
+			var userToDelete = await _userManager.FindByIdAsync(id);
+
+			//Check if user exists with the given ID or not.
+			if(userToDelete != null)
+			{
+				_context.Users.Remove(userToDelete);
+
+				await _context.SaveChangesAsync();
+
+				return NoContent(); //Return 204 NO CONTENT STATUS CODE.
+            }
+			else
+			{
+				return BadRequest("No user was found with the given ID"); //Return 400 BAD REQUEST STATUS CODE.
+            }
+        }
+
+
+		/// <summary>
+		/// This endpoint gets a user using the given email , if the user exists , it adds to the given role
+		/// Also , we check if the role exists or not , in case it doesn't , we return a bad request.
+		/// </summary>
+		/// <param name="email"></param>
+		/// <param name="roleName"></param>
+		/// <returns></returns>
 		[HttpPost("AddUserToRole")]
 		public async Task<IActionResult> AddUserToRole(string email, string roleName)
 		{
@@ -157,6 +202,82 @@ namespace ButterflyStore.Server.Controllers
 
 			return BadRequest(new { Error = "Something went wrong." });
         }
+
+
+		/// <summary>
+		/// This endpoint returns all the roles given to a certain user
+		/// </summary>
+		/// <param name="email">We use the email to fetch the user from the database./param>
+		/// <returns></returns>
+		[HttpGet("GetUserRoles")]
+		public async Task<IActionResult> GetUserRoles(string email)
+		{
+            //Check if any of the received email is NULL or EMPTY.
+            if (string.IsNullOrEmpty(email))
+            {
+                return BadRequest(new { Message = "Please enter a valid email."}); //Return 400 BAD REQUEST STATUS CODE.
+            }
+
+			var user = await _userManager.FindByEmailAsync(email);
+
+			//Check if a user exists with that email or not
+			if(user == null)
+			{
+				return BadRequest(new { Message = "No user was found with the given email." }); //Return 400 BAD REQUEST STATUS CODE.
+            }
+
+			//Retrieve the roles for that user.
+			var userRoles = await _userManager.GetRolesAsync(user);
+
+			return Ok(userRoles); // Return 200 OK STATUS CODE.
+        }
+
+
+        /// <summary>
+        /// This endpoint removes a user from a specifc role ,  , and the role name t
+        /// </summary>
+        /// <param name="email">We use the email to get the user</param>
+        /// <param name="roleName"></param>
+        /// <returns></returns>
+        [HttpPost("RemoveUserFromRole")]
+		public async Task<IActionResult> RemoveUserFromRole(string email , string roleName)
+		{
+            //Check if any of the received data is NULL or EMPTY.
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(roleName))
+            {
+                return BadRequest(); //Return 400 BAD REQUEST STATUS CODE.
+            }
+
+
+			//Check if user exists.
+			var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return BadRequest(new { Message = "No user was found with the given email." }); //Return 400 BAD REQUEST STATUS CODE.
+            }
+
+			//Check if role exists.
+
+			var roleExists = await _roleManager.RoleExistsAsync(roleName);
+
+            if (!roleExists)
+            {
+                return BadRequest(new { Message = "No role exists with the given name" }); //Return 400 BAD REQUEST STATUS CODE.
+            }
+
+			var result = await _userManager.RemoveFromRoleAsync(user, roleName);
+
+			if(result.Succeeded)
+			{
+                return Ok(); // Return 200 OK STATUS CODE.
+            }
+			else
+			{
+				return BadRequest(new { Message = "Something went wrong while attempting to remove the user from the role." }); //Return 400 BAD REQUEST STATUS CODE.
+            }
+        }
+
 	}
 }
 
